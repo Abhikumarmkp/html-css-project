c183a48964ad992793aca4615c6cbe5847d66d69
BHARTIDZZ <134090500+Abhikumarmkp@users.noreply.github.com>
2025-11-16 14:07:13 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,26 @@
+
+let count = 0;
+
+const counter = document.getElementById("counter");
+const incrementBtn = document.getElementById("increment");
+const decrementBtn = document.getElementById("decrement");
+const resetBtn = document.getElementById("reset");
+
+function updateCounter() {
+ counter.textContent = count;
+}
+// Increment button 
+incrementBtn.addEventListener("click", () => {
+ count++;
+ updateCounter();
+});
+// Decrement button 
+decrementBtn.addEventListener("click", () => {
+ if (count > 0) count--; // 
+ updateCounter();
+});
+// Reset button 
+resetBtn.addEventListener("click", () => {
+ count = 0;
+ updateCounter();
+});
